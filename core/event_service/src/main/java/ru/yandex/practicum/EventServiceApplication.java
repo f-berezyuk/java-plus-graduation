684446d0e960ab13fae9c8c8ab2b063b164d8e16
f.berezyuk <f.berezyuk@gmail.com>
2025-04-24 23:31:38 +0500

    everything is running

--- core/event_service/src/main/java/ru/yandex/practicum/EventServiceApplication.java
+++ core/event_service/src/main/java/ru/yandex/practicum/EventServiceApplication.java
@@ -3,9 +3,11 @@ package ru.yandex.practicum;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
 @EnableDiscoveryClient
+@EnableFeignClients(basePackages = "ru.practicum.interaction.feign.client")
 public class EventServiceApplication {
     public static void main(String[] args) {
         SpringApplication.run(EventServiceApplication.class, args);
